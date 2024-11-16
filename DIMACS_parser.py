5e367ed0a6255c60c630ae5f9edc3225eb8c036a
P-PPT <76959908+PT-VU@users.noreply.github.com>
2024-11-17 00:52:04 +0800

    update parser

--- DIMACS_parser.py
+++ DIMACS_parser.py
@@ -1,11 +1,55 @@
 from typing import Dict, List, Set, Tuple
+import numpy as np
+# from DPLL import dpll
+
+def to_DIMACS(size,state_file,rule_file):
+    # the rows num of out_file of 4*4 is 455 = 1 + 6 + 7*(4*4*4)
+    # the rows num of out_file of 9*9 is 12010 = 1 + 21 + 37*(9*9*4)
+
+    #for states
+    with open(state_file,"r") as s:
+        a = s.readlines()
+        # for line in a:
+        state = [list(a[0][size*i:size*(i+1)]) for i in range(size)]
+        board = np.array([["_" if j == "." else j  for j in row] for row in state])
+        state_ind = np.argwhere(board != "_")
+        cnf_list = []
+        for i in state_ind:
+            #take each given number
+            num = board[i[0],i[1]]
+            # cnf form
+            cnf = f"{i[0]+1}{i[1]+1}{num} 0"
+            cnf_list.append(cnf)
+
+    #for rules
+    with open(rule_file, "r") as r:
+        lines = r.readlines()
+        exist_rule = []
+        repetition_rule = []
+        for i in lines:
+            a = i.split(" ")
+
+            if "p" in a:
+                pass
+            elif len(a) == size + 1:
+                exist_rule.append(i)
+            else:
+                repetition_rule.append(i)
+
+    with open(f"{size}by{size}.cnf","w") as t:
+        t.write(f"p cnf {size}{size}{size} {len(cnf_list) + len(exist_rule) + len(repetition_rule)}\n")
+        for i in cnf_list:
+            t.write(f"{i} \n")
+        for i in exist_rule:
+            t.write(f"{i}")
+        for i in repetition_rule:
+            t.write(f"{i}")
 
 def DIMACS_reader(file):
     #within 10*10 puzzle
     with open(file,"r") as f:
         lines = f.readlines()
         para = lines[0].split(" ")[2]
-        print(lines[0].split(" ")[2])
         symbols = set()
         for i in range(100,1000):
             if "0" not in str(i) and str(i)[0] <= para[0] and str(i)[1] <= para[1] and str(i)[2] <= para[2] :
@@ -52,3 +96,18 @@ def save_dimacs(content: str, filename: str):
         file.write(content)
 
     print(f"DIMACS CNF file '{filename}.output' generated successfully.")
+
+
+
+
+# to_DIMACS(9,"1000 sudokus.txt","sudoku-rules-9x9.txt")
+# symbolss, clausess = DIMACS_reader("9by9.cnf")
+# symbols, clauses = DIMACS_reader("4by4.cnf")
+# print(len(symbols),len(clauses))
+# print(len(symbolss),len(clausess))
+# solver, if_solved = dpll({}, clausess, symbolss)
+#
+# print(if_solved)
+# print(solver)
+# dimacs_content = tt_to_dimacs(solver)
+# save_dimacs(dimacs_content,'test_out')
